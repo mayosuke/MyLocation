fea93afbf625e4a79c94eb1315041594365170d9
mayosuke <mayo.suke@gmail.com>
2012-09-01 06:07:05 +0900

    ログ表示用ViewをEditTextに変更した。

--- src/jp/mayosuke/android/mylocation/MainActivity.java
+++ src/jp/mayosuke/android/mylocation/MainActivity.java
@@ -1,20 +1,38 @@
 package jp.mayosuke.android.mylocation;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView mTextView1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView1 = (TextView) findViewById(R.id.textView1);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_get_location:
+            mTextView1.append("現在位置の情報を取得します。\n");
+        case R.id.menu_start_auto_logging:
+            
+        case R.id.menu_settings:
+            
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
