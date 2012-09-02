e73b5bc60358fb65e40836c26d7f99334227c59f
mayosuke <mayo.suke@gmail.com>
2012-09-02 16:22:53 +0900

    手動で現在位置を取得する処理を実装した。

--- src/jp/mayosuke/android/mylocation/MainActivity.java
+++ src/jp/mayosuke/android/mylocation/MainActivity.java
@@ -1,13 +1,18 @@
 package jp.mayosuke.android.mylocation;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private TextView mTextView1;
 
     @Override
@@ -27,12 +32,42 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case R.id.menu_get_location:
-            mTextView1.append("現在位置の情報を取得します。\n");
+            getCurrentLocation();
         case R.id.menu_start_auto_logging:
             
         case R.id.menu_settings:
             
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void getCurrentLocation() {
+        Log.d(TAG, "getCurrentLocation()");
+        mTextView1.append("現在位置の情報を取得します。\n");
+        final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        final Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (lastLocation != null) {
+            mTextView1.append("最後に取得した位置：" + lastLocation.toString() + "\n");
+        }
+        final LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d(TAG, "getCurrentLocation().locationListener.onLocationChanged():location=" + location.toString());
+                mTextView1.append("現在位置：" + location.toString() + "\n");
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d(TAG, "getCurrentLocation().locationListener.onStatusChanged():provider=" + provider + ",status=" + status + ",extras=" + extras);
+            }
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.d(TAG, "getCurrentLocation().locationListener.onProviderEnabled():provider=" + provider);
+            }
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d(TAG, "getCurrentLocation().locationListener.onProviderDisabled():provider=" + provider);
+            }
+        };
+        locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
     }
 }
